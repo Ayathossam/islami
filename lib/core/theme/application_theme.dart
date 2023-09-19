78c5ad852dc1f5b4ed107417a001633fe1649283
Ayathossam <ayathossam853@gmail.com>
2023-09-19 04:10:06 +0300

    add bottom navigation bar

--- lib/core/theme/application_theme.dart
+++ lib/core/theme/application_theme.dart
@@ -14,5 +14,27 @@ class ApplicationTheme {
         color: Colors.black,
       ),
     ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Color(0xFFB7935F),
+      selectedIconTheme: IconThemeData(color: Colors.black, size: 30),
+      selectedItemColor: Colors.black,
+      unselectedIconTheme: IconThemeData(color: Colors.white, size: 30),
+      unselectedItemColor: Colors.white,
+    ),
+  );
+  static ThemeData darkTheme = ThemeData(
+    primaryColor: Color(0xFFB7935F),
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
+      titleTextStyle: GoogleFonts.elMessiri(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+    ),
   );
 }
--- lib/layout/home.dart
+++ lib/layout/home.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomeLayout extends StatelessWidget {
+class HomeLayout extends StatefulWidget {
   static const String routeName = "home_layout";
 
-  const HomeLayout({super.key});
+  HomeLayout({super.key});
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +26,25 @@ class HomeLayout extends StatelessWidget {
           title: Text('إسلامى'),
         ),
         bottomNavigationBar: BottomNavigationBar(
+          onTap: (int index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          currentIndex: selectedIndex,
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/icon_quran.png')),
+                label: 'quran'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/icon_hadeth.png')),
+                label: 'hadeth'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/icon_sebha.png')),
+                label: 'tasbeh'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/icon_radio.png')),
+                label: 'radio'),
             BottomNavigationBarItem(
                 icon: Icon(Icons.settings), label: 'settings'),
           ],
